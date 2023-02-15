e69abe822ff645362b3a1109501855a53f99821e
Tushar Randhe <121237922+tusharrandhe@users.noreply.github.com>
2023-02-15 23:20:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-//your code here
+let todoValue = document.getElementById("newTodoInput");
+let btn = document.getElementById("addTodoBtn");
+let ol = document.getElementById("todoList");
 
+function addTodo()
+{
+	if(todoValue.value)
+	{
+		let li = document.createElement("li");
+		li.textContent = todoValue.value;
+		ol.appendChild(li);
+		todoValue.value = "";
+	}
+}
+btn.addEventListener("click",addTodo);
